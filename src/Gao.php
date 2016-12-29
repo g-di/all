cc2c497147fb72f339a960a1745e6cf65e2c93a6
高迪 <gaodi@we-te.com>
2016-12-29 17:52:10 +0800

    no message

---
+++ src/Gao.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev001
+ * Date: 16-12-29
+ * Time: 下午5:49
+ */
+namespace Gao\Gao;
+
+class Gao{
+
+    public static function Gao(){
+        echo 'Hello World';
+    }
+}
