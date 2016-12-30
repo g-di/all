14b89b9643bae5216922607a2795387b51d3b416
高迪 <gaodi@we-te.com>
2016-12-30 14:42:06 +0800

    缩放图片

--- src/Gao.php
+++ src/Gao.php
@@ -6,10 +6,17 @@
  * Time: 下午5:49
  */
 namespace Gao\Gao;
+use Gao\Gao\SuoImage;
 
 class Gao{
 
     public static function Gao(){
         echo 'Hello World';
     }
+
+    public static function SuoImage($source_path, $name , $target_width=115, $target_height=115)
+    {
+        SuoImage::SuoImage($source_path, $name , $target_width, $target_height);
+    }
+
 }
---
+++ src/all/SuoImage.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev001
+ * Date: 16-12-30
+ * Time: 下午2:34
+ */
+namespace Gao\Gao\SuoImage;
+
+class SuoImage
+{
+
+    public function SuoImage($source_path, $name , $target_width, $target_height)
+    {
+        file_put_contents($name, file_get_contents($source_path));
+        $source_info = getimagesize($source_path);
+        $source_width = $source_info[0];
+        $source_height = $source_info[1];
+        $source_mime = $source_info['mime'];
+        $source_ratio = $source_height / $source_width;
+        $target_ratio = $target_height / $target_width;
+
+        // 源图过高
+        if ($source_ratio > $target_ratio) {
+            $cropped_width = $source_width;
+            $cropped_height = $source_width * $target_ratio;
+            $source_x = 0;
+            $source_y = ($source_height - $cropped_height) / 2;
+        } // 源图过宽
+        elseif
+        ($source_ratio < $target_ratio
+        ) {
+            $cropped_width = $source_height / $target_ratio;
+            $cropped_height = $source_height;
+            $source_x = ($source_width - $cropped_width) / 2;
+            $source_y = 0;
+        } // 源图适中
+        else {
+            $cropped_width = $source_width;
+            $cropped_height = $source_height;
+            $source_x = 0;
+            $source_y = 0;
+        }
+
+        switch ($source_mime) {
+            case 'image/gif':
+                $source_image = imagecreatefromgif($source_path);
+                break;
+
+            case 'image/jpeg':
+                $source_image = imagecreatefromjpeg($source_path);
+                break;
+
+            case 'image/png':
+                $source_image = imagecreatefrompng($source_path);
+                break;
+
+            default:
+                return false;
+                break;
+        }
+
+        $target_image = imagecreatetruecolor($target_width, $target_height);
+        $cropped_image = imagecreatetruecolor($cropped_width, $cropped_height);
+
+// 裁剪
+        imagecopy($cropped_image, $source_image, 0, 0, $source_x, $source_y, $cropped_width, $cropped_height);
+// 缩放
+        imagecopyresampled($target_image, $cropped_image, 0, 0, 0, 0, $target_width, $target_height, $cropped_width, $cropped_height);
+
+        imagejpeg($target_image, $name);
+        imagedestroy($source_image);
+        imagedestroy($target_image);
+        imagedestroy($cropped_image);
+    }
+}
